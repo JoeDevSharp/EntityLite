7da8a67e574608fd9d0afaad1279ed99d7c26222
José Miguel <clasiccode@gmail.com>
2024-09-14 11:59:25 +0200

    Update Entity.php

--- src/Entity.php
+++ src/Entity.php
@@ -2,7 +2,6 @@
 namespace EntityLite;
 
 abstract class Entity {
-    public static string $table;
 
     public function toArray(): array {
         return get_object_vars($this); // Converts the properties to an array
